8477c5fd2ae292a8d1afc1078f942d896f7a691e
rafaapcode <rafaap2003@gmail.com>
2024-04-29 08:30:55 -0300

    feat: implement the shopping-cart class'

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Oi");
+import { ShoppingCart } from "./srp/shopping-cart";
+
+const shoppingCart = new ShoppingCart();
+shoppingCart.addItem({name: "Shorts", price: 90});
+shoppingCart.addItem({name: "PS5", price: 5000});
+shoppingCart.addItem({name: "Iphone 15", price: 5000});
+
+console.log(shoppingCart.total());
