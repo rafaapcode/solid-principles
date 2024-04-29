05a7c9c3e3986fd2b197934f26b1f45a25b894f6
rafaapcode <rafaap2003@gmail.com>
2024-04-29 09:34:18 -0300

    feat: implementing the principle of Single Reponsabilitie on the classes

--- src/srp/shopping-cart.ts
+++ src/srp/shopping-cart.ts
@@ -1,8 +1,7 @@
-type CartItem = { name: string; price: number };
+import { CartItem } from "./interfaces/cart-item";
 
 export class ShoppingCart {
     private readonly _items: CartItem[] = [];
-    private orderStatus: "open" | "closed" = "open";
     
     addItem(item: CartItem): void {
         this._items.push(item);
@@ -20,30 +19,10 @@ export class ShoppingCart {
         return +this._items.reduce((prev, curr) => prev + curr.price,0).toFixed(2);
     }
 
-    checkout():void {
-        if(this.isEmpty()) {
-            console.log("Seu carrinho está vazio");
-            return;
-        }
-
-        this.orderStatus = 'closed';
-        this.sendMessage("Seu pedido foi recebido");
-        this.saveOrder();
-        this.clear();
-    }
-
     isEmpty():boolean {
         return this._items.length === 0;
     }
-
-    sendMessage(msg: string): void {
-        console.log(msg);
-    }
-
-    saveOrder(): void {
-        console.log("Pedido salvo com sucesso !");
-    }
-
+    
     clear(): void {
         this._items.length = 0;
     }
