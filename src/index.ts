808b40a06270d2fcaa399cf1d079f92c7c8f244b
rafaapcode <rafaap2003@gmail.com>
2024-04-29 09:34:43 -0300

    test: making tests with client code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
+import { Messaging } from "./srp/messaging";
+import { Order } from "./srp/order";
+import { Persistency } from "./srp/persistence";
+import { Product } from "./srp/product";
 import { ShoppingCart } from "./srp/shopping-cart";
 
 const shoppingCart = new ShoppingCart();
-shoppingCart.addItem({name: "Shorts", price: 90});
-shoppingCart.addItem({name: "PS5", price: 5000});
-shoppingCart.addItem({name: "Iphone 15", price: 5000});
+const msg = new Messaging();
+const persist = new Persistency();
+const order = new Order(shoppingCart, msg, persist);
+shoppingCart.addItem(new Product("Shorts", 90));
+shoppingCart.addItem(new Product("PS5", 5000));
+shoppingCart.addItem(new Product("Iphone 15", 4550));
 
 console.log(shoppingCart.total());
+console.log(shoppingCart.items);
+console.log(order.orderStatus);
+order.checkout();
+console.log(order.orderStatus);
