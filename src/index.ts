5eff500c84e6b306b04070f7d05413149636d97c
rafaapcode <rafaap2003@gmail.com>
2024-05-01 08:38:11 -0300

    feat: injection of dependency

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { Messaging } from "./srp/services/messaging";
-import { Order } from "./srp/entities/order";
-import { Persistency } from "./srp/services/persistence";
-import { Product } from "./srp/entities/product";
-import { ShoppingCart } from "./srp/entities/shopping-cart";
+import { Messaging } from "./ocp/services/messaging";
+import { Order } from "./ocp/entities/order";
+import { Persistency } from "./ocp/services/persistence";
+import { Product } from "./ocp/entities/product";
+import { ShoppingCart } from "./ocp/entities/shopping-cart";
+import { FifityPercentDiscount } from "./ocp/entities/discount";
 
-const shoppingCart = new ShoppingCart();
+const fiftyPercentDiscount = new FifityPercentDiscount();
+const shoppingCart = new ShoppingCart(fiftyPercentDiscount);
 const msg = new Messaging();
 const persist = new Persistency();
 const order = new Order(shoppingCart, msg, persist);
@@ -13,6 +15,7 @@ shoppingCart.addItem(new Product("PS5", 5000));
 shoppingCart.addItem(new Product("Iphone 15", 4550));
 
 console.log(shoppingCart.total());
+console.log(shoppingCart.totalWithDiscount());
 console.log(shoppingCart.items);
 console.log(order.orderStatus);
 order.checkout();
